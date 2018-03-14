13860796e86a426c5bc20c88c92bc905e5bc3ff4
Desii <diallusions@gmail.com>
2018-03-14 07:09:45 -0400

    boop?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='weeb.py',
       author_email='godavaru@gmail.com',
       description='An API wrapper for weeb.sh',
       url='https://github.com/Desiiii/weeb.py',
-      version='1.0.0',
+      version='1.0.1',
       packages=["weeb"],
       classifiers=[
             'Development Status :: 4 - Beta',
